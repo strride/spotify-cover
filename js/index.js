c7ba54f979b5f4844271264a53e60eabd024a5f3
Olivier Labarre <1840910@csfoy.ca>
2024-11-16 23:04:19 -0500

    Added download functionality

--- js/index.js
+++ js/index.js
@@ -10,7 +10,13 @@ async function fetchSpotifyData(url) {
     }
     const data = await response.json();
     if (data && data.thumbnail_url) {
-      document.getElementById("cover").src = data.thumbnail_url;
+      const coverImg = document.getElementById("cover");
+      coverImg.src = data.thumbnail_url;
+      coverImg.dataset.title = data.title || 'cover';
+
+      coverImg.onload = () => {
+        document.querySelector('.download').style.display = 'inline-block';
+      };
     } else {
       console.error("Cover URL not found in the response");
     }
@@ -18,6 +24,26 @@ async function fetchSpotifyData(url) {
     console.error("Error fetching data:", error);
   }
 }
+
+document.querySelector('.download').addEventListener('click', async function() {
+  const coverImg = document.getElementById('cover');
+  const imageUrl = coverImg.src;
+  if (imageUrl) {
+    try {
+      const response = await fetch(imageUrl);
+      const blob = await response.blob();
+      const blobUrl = window.URL.createObjectURL(blob);
+
+      const link = document.createElement('a');
+      link.href = blobUrl;
+      link.download = `cover_${formatString(coverImg.dataset.title)}.jpg`;
+      document.body.appendChild(link);
+      link.click();
+    } catch (error) {
+      console.error('Error downloading image:', error);
+    }
+  }
+});
 
 document.getElementById("submit").addEventListener("click", function () {
   var url = document.getElementById("input").value;
@@ -27,3 +53,8 @@ document.getElementById("submit").addEventListener("click", function () {
     alert("Please enter a valid Spotify URL");
   }
 });
+
+function formatString(str) {
+  return str.replace(/ \([^)]*\)|'| /g, '_');
+}
+
